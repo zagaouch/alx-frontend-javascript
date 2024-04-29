fe1d29bdf234192287386f9ebc47d2e4b985fd83
zagaouch <i.zagaouch@gmail.com>
2024-04-29 16:58:18 +0100

    auto commit

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, bodu: 'success' }))
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
