7b6478a7b681c0811922401f0222c6a410586864
zagaouch <i.zagaouch@gmail.com>
2024-04-29 17:31:24 +0100

    auto commit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
+
     .catch(() => console.log('Signup system offline'));
 }
